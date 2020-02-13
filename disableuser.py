234a3b7b90df1082db4c1aaedae83beacbe288b7
coinerwh <coinerwh@gmail.com>
2020-02-12 17:51:03 -0800

    Disable User Master

--- disableuser.py
+++ disableuser.py
@@ -3,9 +3,7 @@
 # Disables user in all cloud systems.
 # Requires source database list to be a CSV with 4 columns for driver, server, uid, and pwd
 
-
 import pyodbc
-import pandas as pd
 import csv
 
 def disable_user(username,list_of_dbs):
@@ -17,13 +15,16 @@ def disable_user(username,list_of_dbs):
                         "Database="+db[1]+";"
                         "uid="+db[2]+";pwd="+db[3])
             crsr = cnxn.cursor()
-            sql_command = "SELECT * FROM Principal WHERE loginname ='" + username + "'"
+            sql_command = "UPDATE Principal SET IsEnabled = 'False' WHERE loginname ='" + username + "'"
             crsr.execute(sql_command)
             print(crsr.rowcount, 'users disabled')
             cnxn.commit()
             crsr.close()
             cnxn.close()
             print("SQL executed successfully, disabled",username,"from",db[1])
+            sqlresults = str("SQL executed successfully, disabled " + username + " from " + db[1] + '\n')
+            with open('SQL_Results.txt', 'w') as outfile:
+                outfile.write(sqlresults)
         except pyodbc.Error as ex:
             sqlstate = ex.args[0]
             if sqlstate == '28000':
@@ -31,9 +32,8 @@ def disable_user(username,list_of_dbs):
 
 
 def main():
-    bad_credentials = True
-    while bad_credentials == True:
-        databasename = str(input("Enter the full file path of database file: "))
+    databasename = str(input("Enter the full file path of database file: "))
+    while  1 == 1:
         username = str(input("Enter the username: "))
         with open(databasename, 'r') as infile:
             reader = csv.reader(infile)
@@ -45,29 +45,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-### Code that writes to file if successful. Loops if unable to execute ###
 
-# if sqlresults == None:
-    # bad_credentials = True
-# else:
-    # print(sqlresults)
-    # with open('SQL_Results.txt','w') as outfile:
-        #outfile.write(sqlresults)
-    # bad_credentials = False
-
-### Dynamic and hardcoded connection code ###
-
-#pyodbc.connect("Driver={SQL Server};"
-                    #"Server="+db[0]+";"
-                    #"Database="+db[1]+";"
-                    #"uid="+db[2]+";pwd="+db[3])
-
-#cnxn = pyodbc.connect("Driver={SQL Server};"
-                                  #"Server=axiomepmdb01.database.windows.net;"
-                                  #"Database=AxiomSupportTraining;"
-                                  #"uid=support_wcoiner;pwd=RhFn++bG69$m,N70")
-
-#query = "SELECT * FROM Principal WHERE loginname ='" + username + "'"
-            #return pd.read_sql_query(query, cnxn)
-
-#"UPDATE Principal SET IsEnabled = FALSE WHERE loginname ='" + username + "'"
